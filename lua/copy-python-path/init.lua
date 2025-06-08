cbf9d4fb46303ee78e01f6977b05d0e9f25eae9b
AnsonH <ansonheung2000@gmail.com>
2025-06-08 15:54:15 +0800

    chore: remove boilerplate code

--- lua/copy-python-path/init.lua
+++ lua/copy-python-path/init.lua
@@ -1,36 +1,11 @@
-local main = require("copy-python-path.main")
-local config = require("copy-python-path.config")
+local M = {}
 
-local CopyPythonPath = {}
+function M.copy_dotted_path()
+    print("TODO")
 
---- Toggle the plugin by calling the `enable`/`disable` methods respectively.
-function CopyPythonPath.toggle()
-    if _G.CopyPythonPath.config == nil then
-        _G.CopyPythonPath.config = config.options
-    end
+    -- TODO: Early return if not `*.py` file
 
-    main.toggle("public_api_toggle")
+    -- TODO: Get workspace folder root
 end
 
---- Initializes the plugin, sets event listeners and internal state.
-function CopyPythonPath.enable(scope)
-    if _G.CopyPythonPath.config == nil then
-        _G.CopyPythonPath.config = config.options
-    end
-
-    main.toggle(scope or "public_api_enable")
-end
-
---- Disables the plugin, clear highlight groups and autocmds, closes side buffers and resets the internal state.
-function CopyPythonPath.disable()
-    main.toggle("public_api_disable")
-end
-
--- setup CopyPythonPath options and merge them with user provided ones.
-function CopyPythonPath.setup(opts)
-    _G.CopyPythonPath.config = config.setup(opts)
-end
-
-_G.CopyPythonPath = CopyPythonPath
-
-return _G.CopyPythonPath
+return M
--- plugin/copy-python-path.lua
+++ plugin/copy-python-path.lua
@@ -1,15 +1,8 @@
--- You can use this loaded variable to enable conditional parts of your plugin.
-if _G.CopyPythonPathLoaded then
-    return
-end
-
-_G.CopyPythonPathLoaded = true
-
 -- Useful if you want your plugin to be compatible with older (<0.7) neovim versions
 if vim.fn.has("nvim-0.7") == 0 then
     vim.cmd("command! CopyPythonPath lua require('copy-python-path').toggle()")
 else
     vim.api.nvim_create_user_command("CopyPythonPath", function()
-        require("copy-python-path").toggle()
+        require("copy-python-path").copy_dotted_path()
     end, {})
 end
